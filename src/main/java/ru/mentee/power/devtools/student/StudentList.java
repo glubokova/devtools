1c1f4fe0a61d25ce74e24937d0ca229b7215a96b
Aida Glubokova <glubokova_aida@mail.ru>
2026-01-28 19:04:43 +0500

    DVT-7: Checkstyle и культура кода — автоматизация стандартов

---
+++ src/main/java/ru/mentee/power/devtools/student/StudentList.java
@@ -0,0 +1,22 @@
+package ru.mentee.power.devtools.student;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentList {
+  private List<Student> studentList;
+
+  public StudentList() {
+    studentList = new ArrayList<>();
+  }
+
+  public void addStudent(Student student) {
+    if (student != null) {
+      studentList.add(student);
+    }
+  }
+
+  public List<Student> getStudentByCity(String city) {
+    return studentList.stream().filter(s -> s.city().equals(city)).toList();
+  }
+}
